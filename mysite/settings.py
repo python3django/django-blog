9dcf6ac15259e6f7fd7d87400822208d05ccd6fd
python3django <python3github@gmail.com>
2019-04-05 19:39:32 +0300

    Added pagination and blog/api/README.md for API

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,5 +156,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication', 
     ),
     #'DEFAULT_PERMISSION_CLASSES':  ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 4,
 }
 
