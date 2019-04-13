2aa973403c388628cbcc5d99acad0609fde8c077
python3django <python3github@gmail.com>
2019-04-13 18:28:44 +0300

    Edited settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
-  	'account.apps.AccountConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
